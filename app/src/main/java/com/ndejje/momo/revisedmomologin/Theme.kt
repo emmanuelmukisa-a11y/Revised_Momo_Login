2a78302045b3c6d2e099f6095fe62c8c4fbb5323
emmanuelmukisa-a11y <emmanuel.mukisa@stud.ndejjeuniversity.ac.ug>
2026-04-27 07:20:16 +0300

    done with theme. simple app ready

---
+++ app/src/main/java/com/ndejje/momo/revisedmomologin/Theme.kt
@@ -0,0 +1,13 @@
+package com.ndejje.momo.revisedmomologin.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RevisedMoMoLoginTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        content = content
+    )
+}
